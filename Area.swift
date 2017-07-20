8ff00e62d545e18a16927b2acc04562c4ee4d5dd
happts <happts@qq.com>
2017-07-20 19:24:40 +0800

    test
    尝试使用

---
+++ Area.swift
@@ -0,0 +1,26 @@
+//
+//  Area.swift
+//  FansArea
+//
+//  Created by happts on 17/7/8.
+//  Copyright © 2017年 happts. All rights reserved.
+//
+
+import Foundation
+
+struct Area {
+    var name :String
+    var provicve : String
+    var part : String
+    var image : String
+    var isVisited : Bool
+    var rating = ""
+    
+    init(name: String, provicve: String, part: String, image: String, isVisited: Bool) {
+        self.name = name
+        self.provicve = provicve
+        self.part = part
+        self.image = image
+        self.isVisited = isVisited
+    }
+}
